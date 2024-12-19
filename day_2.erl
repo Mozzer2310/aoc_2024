24511f8b34454b24473f049ace507ffebbbba4bd
Mozzer2310 <sammorris2310@hotmail.com>
2024-12-19 17:53:23 +0000

    Complete day 2 part 2

--- day_2.erl
+++ day_2.erl
@@ -1,7 +1,12 @@
 -module(day_2).
+%% Solves the Day 1: Historian Hysteria problem: https://adventofcode.com/2024/day/2#part2
+%% Summary:
+%%  - Part 1: Finds the total number of safe reports
+%%  - Part 2: Finds the total number of safe reports with the problem dampener
 
 -export([
-  part_1/0, 
+  part_1/0,
+  part_2/0,
   read_input/1, 
   total_safe_reports/1
 ]).
@@ -12,6 +17,12 @@ part_1() ->
   Reports = read_input("input/day_2.txt"),
   total_safe_reports(Reports).
 
+%% Completes part 2 in one call
+-spec part_2() -> integer().
+part_2() ->
+  Reports = read_input("input/day_2.txt"),
+  total_safe_reports_problem_dampener(Reports).
+
 %% Reads the input file into correctly formatted 'Reports'
 -spec read_input(string()) -> {list(), list()}.
 read_input(FileName) ->
@@ -20,6 +31,7 @@ read_input(FileName) ->
   format_lines(Lines, []).
 
 %% Determines the total number of 'safe' reports from a list of 'Reports'
+-spec total_safe_reports(list()) -> integer().
 total_safe_reports(Reports) ->
   lists:foldl(
     fun(Report, Count) ->
@@ -31,9 +43,42 @@ total_safe_reports(Reports) ->
       end
     end, 0, Reports).
 
+%% Determines the total number of 'safe' reports from a list of 'Reports' 
+%% with the Problem Dampener feature
+total_safe_reports_problem_dampener(Reports) ->
+  lists:foldl(
+    fun(Report, Count) ->
+      case is_safe_report(Report) of
+        true -> 
+          Count + 1;
+         false -> 
+          case is_safe_problem_dampened(Report) of
+            true ->
+              Count +1;
+            false ->
+              Count
+          end
+      end
+    end, 0, Reports).
+
 %% Checks if a single report is 'safe', true for all conditions
 is_safe_report(Report) ->
   is_increase_or_decrease(Report) and is_safe_difference(Report).
+
+is_safe_problem_dampened(Report) ->
+  is_safe_problem_dampened([], Report).
+
+%%  Checks if the Report is safe when removing 1 element, checks with removing each element
+%% in order, if true it terminates early
+is_safe_problem_dampened(_Left, []) ->
+  false;
+is_safe_problem_dampened(Left, [E | Right]) ->
+  case is_safe_report(Left ++ Right) of
+    true ->
+      true;
+    false ->
+      is_safe_problem_dampened(Left ++ [E], Right)
+    end.
 
 %% Checks if a report is increasing or decreasing
 is_increase_or_decrease(Report) ->
